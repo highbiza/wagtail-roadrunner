9c0c0da5c5abbe2abc212825bef4eb5aca031227
Stefan Jilsink <stefan@uwkm.nl>
2019-02-05 16:03:47 +0100

    try again

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 import codecs
 from os import path
@@ -7,7 +8,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with codecs.open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 extras_require: dict = {
@@ -21,16 +22,16 @@ extras_require: dict = {
 }
 
 setup(
-    name='roadrunner',
-    version='2.1.1',
-    description='RoadRunner',
+    name="roadrunner",
+    version="2.1.1",
+    description="RoadRunner",
     long_description=long_description,
-    url='https://gitlab.com/uwkm-frets/RoadRunner',
-    branch_url='https://gitlab.com/uwkm-frets/RoadRunner/tree/master',
-    download_url='https://gitlab.com/uwkm-frets/RoadRunner/repository/archive.zip',
-    author='UWKM',
-    author_email='support@uwkm.nl',
-    license='MIT',
+    url="https://gitlab.com/uwkm-frets/RoadRunner",
+    branch_url="https://gitlab.com/uwkm-frets/RoadRunner/tree/master",
+    download_url="https://gitlab.com/uwkm-frets/RoadRunner/repository/archive.zip",
+    author="UWKM",
+    author_email="support@uwkm.nl",
+    license="MIT",
     include_package_data=True,
     packages=find_packages(),
     classifiers=[
@@ -41,7 +42,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Wagtail :: 2",
     ],
-    keywords='RoadRunner',
+    keywords="RoadRunner",
     install_requires=["wagtail", "wagtailfontawesome"],
     extras_require=extras_require,
 )
