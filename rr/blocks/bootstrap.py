8a8411a2d4f5fc3cb2369b16dc821d381d60a1ca
Lars van de Kerkhof <lars@permanentmarkers.nl>
2022-10-14 16:56:42 +0200

    kdhajkhdw

--- rr/blocks/bootstrap.py
+++ rr/blocks/bootstrap.py
@@ -121,7 +121,7 @@ class TabChildrenBlock(blocks.StructBlock):
 
 class TabBlock(blocks.StructBlock):
     tab_style = BootstrapTabStylingBlock(label="Tab style", default="tabs")
-    tabs = blocks.ListBlock(TabChildrenBlock(form_classname="nav-link"), form_classname="nav nav-tabs mb-3")
+    tabs = blocks.ListBlock(TabChildrenBlock(form_classname="nav-link"), form_classname="nav nav-tabs")
     styling = StylingBlock()
 
     def get_uuid(self):
