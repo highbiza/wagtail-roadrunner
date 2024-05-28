5de8e7cf6c850f08b139a75000f7e21475df10e3
Voxin Muyli <lars@permanentmarkers.nl>
2024-05-28 14:28:51 +0200

    Use the wagtail picture tag to render a picture (#42)
    * Use the wagtail picture tag to render a picture
    
    * Allow svg for images
    
    * Removed useless code

--- rr/widgets.py
+++ rr/widgets.py
@@ -1,7 +1,11 @@
 import json
+import logging
+import traceback
 from wagtail.blocks.base import BlockWidget
 from django.utils.html import format_html
 from .telepath import RoadRunnerJSContext
+
+logger = logging.getLogger(__name__)
 
 
 class RoadRunnerBlockWidget(BlockWidget):
@@ -12,39 +16,12 @@ class RoadRunnerBlockWidget(BlockWidget):
 
     # pylint: disable=attribute-defined-outside-init
     def _build_block_json(self):
-        self._js_context = RoadRunnerJSContext()
-        self._block_json = json.dumps(self._js_context.pack(self.block_def))
-
-    def render(self, name, value, attrs=None, renderer=None):
-        content = super().render(name, value, attrs, renderer)
-        return format_html(
-            """
-        <div id="roadrunner-breakpoint-switcher">
-            <ul>
-                <li>
-                    <button type="button" value="col" class="c-sf-block__actions__single" title="mobile">
-                        <svg class="icon icon-mobile default" aria-hidden="true" focusable="false">
-                            <use href="#icon-mobile"/>
-                        </svg>
-                    </button>
-                </li>
-                <li>
-                    <button type="button" value="col-md" class="c-sf-block__actions__single" title="tablet">
-                        <svg class="icon icon-tablet default" aria-hidden="true" focusable="false">
-                            <use href="#icon-tablet"/>
-                        </svg>
-                    </button>
-                </li>
-                <li>
-                    <button type="button" value="col-lg" class="c-sf-block__actions__single active" title="screen">
-                        <svg class="icon icon-tv default" aria-hidden="true" focusable="false">
-                            <use href="#icon-tv"/>
-                        </svg>
-                    </button>
-                </li>
-            </ul>
-        </div>
-        {content}
-        """,
-            content=content,
-        )
+        try:
+            self._js_context = RoadRunnerJSContext()
+            self._block_json = json.dumps(self._js_context.pack(self.block_def))
+        except Exception as e:
+            logger.error(
+                "Something went wrong while rendering the block json", e, type(e)
+            )
+            traceback.print_exception(e)
+            raise
