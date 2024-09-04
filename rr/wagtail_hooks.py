cb2768835fcc4c551a6283f7533db3d39fb7787e
Lars van de Kerkhof <lars@permanentmarkers.nl>
2024-09-04 15:27:47 +0200

    Because Template is an esoteric feature, we move it under settings.
    This will also allow hiding the snippets menu item ;)

--- rr/wagtail_hooks.py
+++ rr/wagtail_hooks.py
@@ -1,4 +1,18 @@
+from django.utils.translation import gettext_lazy as _
 from wagtail import hooks
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
+
+from .models import Template
+
+
+@register_snippet
+class RoadrunnerTemplateSnippetViewSet(SnippetViewSet):
+    model = Template
+    icon = "doc-full-inverse"
+    menu_label = _("RoadRunner Templates")
+    menu_name = "rr-template"
+    add_to_settings_menu = True
 
 
 @hooks.register("register_icons")
