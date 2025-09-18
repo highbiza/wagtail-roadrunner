3d4c21fb68a357cfa68d02a65feb2589e83c1f12
Viggo de Vries <viggo@highbiza.nl>
2025-09-18 13:18:36 +0200

    no uppercase in comment

--- js/preview/fieldblock.js
+++ js/preview/fieldblock.js
@@ -20,7 +20,7 @@ export class PreviewFieldBlockPreview extends Preview {
 export class PreviewFieldBlockDefinition extends window.wagtailStreamField.blocks.FieldBlockDefinition {
   render(placeholder, prefix, initialState, initialError, parentCapabilities) {
     // unfortunately wagtail did break the rendering of children of a structblock.
-    // Children of strucblocks don;t get any labels anymore, but there is stil code
+    // children of strucblocks don;t get any labels anymore, but there is stil code
     // that checks for idForLabel and if it is set, it will try to access the label.
     // this causes a big error and breaks all structblocks with previews.
     // fornow we solve this by explicitly removing all the label id's for blocks with preview.
