9fc1e112b06187fae7c7ea2d8d40055c9ef5c265
Lars van de Kerkhof <lars@permanentmarkers.nl>
2025-09-18 11:51:27 +0200

    Fixes problems with structblocks with preview in wagtail-roadrunner

--- js/preview/fieldblock.js
+++ js/preview/fieldblock.js
@@ -18,6 +18,17 @@ export class PreviewFieldBlockPreview extends Preview {
 }
 
 export class PreviewFieldBlockDefinition extends window.wagtailStreamField.blocks.FieldBlockDefinition {
+  render(placeholder, prefix, initialState, initialError, parentCapabilities) {
+    // unfortunately wagtail did break the rendering of children of a structblock.
+    // Children of strucblocks don;t get any labels anymore, but there is stil code
+    // that checks for idForLabel and if it is set, it will try to access the label.
+    // this causes a big error and breaks all structblocks with previews.
+    // fornow we solve this by explicitly removing all the label id's for blocks with preview.
+    const renderedBlock = super.render(placeholder, prefix, initialState, initialError, parentCapabilities)
+    renderedBlock.idForLabel = null
+    return renderedBlock
+  }
+
   renderPreview(previewPlaceholder, prefix, initialState, initialError) {
     return new PreviewFieldBlockPreview(this, previewPlaceholder, prefix, initialState, initialError)
   }
