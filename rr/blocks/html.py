d56680f7ff292ae0691393aeff1b92985bd266dd
Viggo de Vries <viggo@highbiza.nl>
2025-11-07 11:11:25 +0100

    Load iframes with loading="lazy"

--- rr/blocks/html.py
+++ rr/blocks/html.py
@@ -123,9 +123,7 @@ class VideoBlockValue(StructValue):
         video_soup.div.iframe["width"] = "100%"
         del video_soup.div.iframe["height"]
         if self.get("lazy"):
-            video_soup.div.iframe["data-src"] = video_soup.div.iframe["src"]
-            video_soup.div.iframe["class"] = "lazy"
-            del video_soup.div.iframe["src"]
+            video_soup.div.iframe["loading"] = "lazy"
         return video_soup
 
 
